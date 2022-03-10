72dcb64a6eca028b667aeeb556c392c2d674a44c
1616imuimu <yagasaki.kei@gmail>
2022-03-10 14:27:10 +0900

    commit

---
+++ patch.js
@@ -0,0 +1,19 @@
+try {
+    setTimeout(() => {
+        const section = document.querySelector('section')
+        let targets = document.querySelectorAll("article a.r-1cvl2hr")
+        targets.forEach(target => target.classList.remove('r-1cvl2hr'))
+
+        const observer = new MutationObserver( () => {
+            targets = document.querySelectorAll("article a.r-1cvl2hr")
+            targets.forEach(target => target.classList.remove('r-1cvl2hr'))
+        })
+        
+        observer.observe(section, {
+            childList: true,
+            subtree: true
+        })
+    }, 1000)
+} catch (error) {
+    console.log("予期せぬエラーが発生しました")
+}
